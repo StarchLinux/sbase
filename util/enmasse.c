b442670b7ce59ec35e3d375d349437c1e18bc9d2
strake <strake888@gmail.com>
2012-10-23 18:30:57 -0500

    enmasse: not die on failure

--- util/enmasse.c
+++ util/enmasse.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/stat.h>
 #include "../util.h"
 
@@ -11,7 +12,7 @@ void
 enmasse(int argc, char **argv, int (*fn)(const char *, const char *))
 {
 	char *buf, *dir;
-	int i;
+	int i, pid, c, n = 0;
 	long size;
 	struct stat st;
 
@@ -22,11 +23,30 @@ enmasse(int argc, char **argv, int (*fn)(const char *, const char *))
 	else
 		dir = (argc == 1) ? "." : argv[--argc];
 
+	pid = fork();
+	if(pid) {
+		waitpid(pid, &n, 0);
+		if (n == 0) return;
+		exit(n);
+	}
+	setpgrp();
 	apathmax(&buf, &size);
 	for(i = 0; i < argc; i++) {
 		if(snprintf(buf, size, "%s/%s", dir, basename(argv[i])) > size)
 			eprintf("%s/%s: filename too long\n", dir, basename(argv[i]));
-		fnck(argv[i], buf, fn);
+		pid = fork();
+		if(pid == 0) {
+			fnck(argv[i], buf, fn);
+			exit(0);
+		}
 	}
 	free(buf);
+	for(;;) {
+		if(waitpid(0, &c, 0) < 0) switch(errno) {
+		case EINTR:	continue;
+		case ECHILD:	exit(n);
+		default:	eprintf("Wait failed\n");
+		}
+		if(c != 0) n++;
+	}
 }
