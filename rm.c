1dec131ed8ef26d4c574daeeeda1c0fbbb6d4de5
Strake <strake888@gmail.com>
2013-01-19 09:20:38 -0500

    rm: recognize -R flag as synonym of -r, as scripts want it

--- rm.c
+++ rm.c
@@ -14,11 +14,12 @@ main(int argc, char *argv[])
 
 	while((c = getopt(argc, argv, "fr")) != -1)
 		switch(c) {
-		case 'f':
-			rm_fflag = true;
-			break;
+		case 'R':
 		case 'r':
 			rm_rflag = true;
+			break;
+		case 'f':
+			rm_fflag = true;
 			break;
 		default:
 			exit(EXIT_FAILURE);
