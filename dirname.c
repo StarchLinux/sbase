62086521fe270d59804b3f7479c4d1358cbf7684
sometwo <dhubdwhhu@yopmail.com>
2013-03-02 16:47:43 -0300

    Update dirname.c

--- dirname.c
+++ dirname.c
@@ -1,18 +1,38 @@
-/* See LICENSE file for copyright and license details. */
-#include <libgen.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include "util.h"
+#include <string.h>
+#include <stdio.h>
 
-int
-main(int argc, char *argv[])
+main(int argc, char **argv)
 {
-	if(getopt(argc, argv, "") != -1)
-		exit(EXIT_FAILURE);
-	if(optind != argc-1)
-		eprintf("usage: %s string\n", argv[0]);
+        char *f, *s;
+        int n;
 
-	puts(dirname(argv[optind]));
-	return EXIT_SUCCESS;
+        if(argc != 2){
+                fprintf(stderr, "Usage: dirname string\n");
+                exit(1);
+        }
+        s = argv[1];
+        f = s + strlen(s) - 1;
+        while(f > s && *f == '/')
+                f--;
+        *++f = 0;
+        /* now f is after last char of string, trailing slashes removed */
+
+        for(; f >= s; f--)
+                if(*f == '/'){
+                        f++;
+                        break;
+                }
+        if(f < s) {
+                *s = '.';
+                s[1] = 0;
+        } else {
+                --f;
+                while(f > s && *f == '/')
+                        f--;
+                f[1] = 0;
+        }
+
+        printf("%s\n", s);
+        return 0;
 }
