c54bb98f58a7116bb5dd4bbd4378c2b59e4734c0
sometwo <dhubdwhhu@yopmail.com>
2013-03-02 16:48:45 -0300

    Update uname.c

--- uname.c
+++ uname.c
@@ -1,58 +1,69 @@
-/* See LICENSE file for copyright and license details. */
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/utsname.h>
-#include "util.h"
 
-int
-main(int argc, char *argv[])
+#define ARGBEGIN        for((argv0=*argv),argv++,argc--;\
+                            argv[0] && argv[0][0]=='-' && argv[0][1];\
+                            argc--, argv++) {\
+                                char *_args, *_argt, _argc;\
+                                _args = &argv[0][1];\
+                                if(_args[0]=='-' && _args[1]==0){\
+                                        argc--; argv++; break;\
+                                }\
+                                while(*_args) switch(_argc=*_args++)
+#define ARGEND          }
+#define ARGF()          (_argt=_args, _args="",\
+                                (*_argt? _argt: argv[1]? (argc--, *++argv): 0))
+#define ARGC()          _argc
+
+char *argv0;
+
+static int started;
+
+static void
+prword(char *w)
 {
-	bool mflag = false;
-	bool nflag = false;
-	bool rflag = false;
-	bool sflag = false;
-	bool vflag = false;
-	char c;
-	struct utsname u;
+        if (started)
+                putchar(' ');
+        else
+                started = 1;
+        fputs(w, stdout);
+}
 
-	while((c = getopt(argc, argv, "amnrsv")) != -1)
-		switch(c) {
-		case 'a':
-			mflag = nflag = rflag = sflag = vflag = true;
-			break;
-		case 'm':
-			mflag = true;
-			break;
-		case 'n':
-			nflag = true;
-			break;
-		case 'r':
-			rflag = true;
-			break;
-		case 's':
-			sflag = true;
-			break;
-		case 'v':
-			vflag = true;
-			break;
-		default:
-			exit(EXIT_FAILURE);
-		}
-	if(uname(&u) == -1)
-		eprintf("uname:");
+void
+main(int argc, char **argv)
+{
+        struct utsname u;
 
-	if(sflag || !(nflag || rflag || vflag || mflag))
-		putword(u.sysname);
-	if(nflag)
-		putword(u.nodename);
-	if(rflag)
-		putword(u.release);
-	if(vflag)
-		putword(u.version);
-	if(mflag)
-		putword(u.machine);
-	putchar('\n');
-	return EXIT_SUCCESS;
+        uname(&u);
+        if(argc == 1){
+                printf("%s\n", u.sysname);
+                exit(0);
+        }
+        ARGBEGIN {
+        case 'a':
+                prword(u.sysname);
+                prword(u.nodename);
+                prword(u.release);
+                prword(u.version);
+                prword(u.machine);
+                break;
+        case 'm':
+                prword(u.machine);
+                break;
+        case 'n':
+                prword(u.nodename);
+                break;
+        case 'r':
+                prword(u.release);
+                break;
+        case 's':
+                prword(u.sysname);
+                break;
+        case 'v':
+                prword(u.version);
+                break;
+        } ARGEND
+        printf("\n");
+        exit(0);
 }
